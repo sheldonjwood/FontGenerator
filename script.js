64c381626eed676055ba14e6d73c50d70d7e3d9a
Sheldon Wood <sheldonjwood@gmail.com>
2019-05-29 23:47:47 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function updateName(){
+    let name = $("#name").val();
+
+    $("#font-1").text("1 - "+name);
+    $("#font-2").text("2 - "+name);
+    $("#font-3").text("3 - "+name);
+    $("#font-4").text("4 - "+name);
+    $("#font-5").text("5 - "+name);
+    $("#font-6").text("6 - "+name);
+    $("#font-7").text("7 - "+name);
+    $("#font-8").text("8 - "+name);
+    $("#font-9").text("9 - "+name);
+    $("#font-10").text("10 - "+name);
+    $("#font-11").text("11 - "+name);
+    $("#font-12").text("12 - "+name);
+    $("#font-13").text("13 - "+name);
+    $("#font-14").text("14 - "+name);
+    $("#font-15").text("15 - "+name);
+}
